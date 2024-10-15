13579cdef2462b594a8d6b8b79b122c0556cb065
Patrick Griffin <paddygriffin1995@gmail.com>
2024-10-15 19:45:31 +0100

    Simple If statement

---
+++ Testing.java
@@ -0,0 +1,13 @@
+public class Testing {
+
+    public String checkDigit(int digit){
+        if (digit > 0){
+            return "Positive";
+        } else if(digit > 0){
+            return "negative";
+        } else{
+            return "null";
+        }
+    }
+
+}
